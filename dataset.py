2bc7ce6db25319c803349d4323277813a9eb7580
TaehwanK-silvia <taehwan@silviahealth.com>
2024-12-09 14:58:42 +0900

    Update v2

--- dataset.py
+++ dataset.py
@@ -6,15 +6,141 @@ from PIL import Image
 from copy import deepcopy
 from torchvision import transforms
 from torch.utils.data import Dataset
+from sklearn.model_selection import train_test_split
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
 # DATASET_DIR = '/Users/taehwan/Documents/Dataset/CDT/MCI-multiple-drawings-main/' # Mac
 DATASET_DIR = '/home/taehwan/Dataset/CDT/MCI-multiple-drawings-main/' # server
 
+class Conv_Att_MCI_Dataset_v2(Dataset):
+    def __init__(self, img_type, label_type):
+        assert type(img_type) == list
+        assert type(label_type) == str and label_type in ['hard', 'soft'], f"label_type is only valid with 'hard' or 'soft', but given {label_type}"
+        self.img_type = img_type
+        self.label_type = label_type
+        self.load_dataset()
+        self.img_normalization = {'mean': [0.485, 0.456, 0.406], 'std':[0.229, 0.224, 0.225]}
+        
+    def load_dataset(self):
+        image_folder = os.path.join(DATASET_DIR, 'images')
+        info_path = os.path.join(DATASET_DIR, 'label.csv')
+        info = pd.read_csv(info_path)
+
+        dataset = {}
+        for patient_id in os.listdir(image_folder):
+            if not patient_id.isdigit():
+                continue
+            
+            _patient_folder = os.path.join(image_folder, patient_id)
+            image_paths = [os.path.join(_patient_folder, img_file) for img_file in os.listdir(_patient_folder) if os.path.splitext(img_file)[-1] == '.png']
+            
+            mask_patient = info['patient_id'] == int(patient_id)
+            assert sum(mask_patient) == 1
+            moca_score = info[mask_patient]['MoCA_score'].iloc[0]
+            score_soft = 1 - sigmoid(moca_score - 24.5)
+            
+            dataset[int(patient_id)] = {os.path.basename(_path).split('.')[0]:_path for _path in image_paths}
+            dataset[int(patient_id)]['score_hard'] = moca_score
+            dataset[int(patient_id)]['label_hard'] = int(moca_score < 25) # MIC
+            dataset[int(patient_id)]['score_soft'] = score_soft.astype('float32')
+            dataset[int(patient_id)]['label_soft'] = int(score_soft >= 0.5) # MCI
+
+        self.dataset_raw = pd.DataFrame(dataset).T        
+        
+    def split_trn_val_test(self):
+        X = self.dataset_raw.loc[:, self.img_type]
+        y = self.dataset_raw.loc[:, [f'score_{self.label_type}', f'label_{self.label_type}']]
+        X_trn_val, X_test, y_trn_val, y_test = train_test_split(X, y, stratify=y.loc[:, f'label_{self.label_type}'], test_size=0.15)
+        X_trn, X_val, y_trn, y_val = train_test_split(X_trn_val, y_trn_val, stratify=y_trn_val.loc[:, f'label_{self.label_type}'], test_size=0.15)
+        dataset_trn = pd.concat([X_trn, y_trn], axis=1)
+        dataset_val = pd.concat([X_val, y_val], axis=1)
+        dataset_test = pd.concat([X_test, y_test], axis=1)
+        
+        dataset_trn = self.load_images(dataset_trn, augmentation=True)
+        dataset_val = self.load_images(dataset_val)
+        dataset_test = self.load_images(dataset_test)
+        
+        return self._split(dataset_trn, 'trn'), self._split(dataset_val, 'val'), self._split(dataset_test, 'test')
+    
+    def get_augmentation(self, x):
+        transform = transforms.Compose([
+            transforms.Pad([12,12,12,12], fill=255), 
+            # left, top, right, bottom, fill=255 to match the background color of original images
+            transforms.RandomCrop([256, 256]),
+        ])
+        return transform(x)
+    
+    def inverse_transform(self, img_torch):
+        assert img_torch.shape[0] == 3, f"Image shape should be (channel, H, W), given {img_torch.shape}"
+        
+        img = img_torch.cpu().numpy()
+        mean = np.array(self.img_normalization['mean'])[:,None,None]
+        std = np.array(self.img_normalization['std'])[:,None,None]
+
+        img_inv = img * std + mean
+        img_inv = np.clip(img_inv, 0, 1)
+        img_inv = img_inv.transpose(1,2,0)
+        return img_inv
+    
+    def load_images(self, dataset_raw, augmentation=False):
+        transform = transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=self.img_normalization['mean'], 
+                                         std=self.img_normalization['std'])
+                ])
+        
+        dataset = {}
+        for patient_id, row in dataset_raw.iterrows():
+            dataset[patient_id] = {'image':{}}
+            if augmentation:
+                dataset[f'{patient_id}_aug'] = {'image':{}}
+                
+            for _type in self.img_type:
+                _img_path = row[_type]
+                _score, _label = row[f'score_{self.label_type}'], row[f'label_{self.label_type}']
+                
+                _img = Image.open(_img_path)
+                assert _img.size == (256,256)
+                img = transform(_img)
+                dataset[patient_id]['image'].update({_type:img})
+                dataset[patient_id]['label'] = _label
+                dataset[patient_id]['score'] = _score
+                
+                if augmentation:
+                    _img_aug = _img.copy()
+                    img_aug = transform(self.get_augmentation(_img_aug))
+                    dataset[f'{patient_id}_aug']['image'].update({_type:img_aug})
+                    dataset[f'{patient_id}_aug']['label'] = _label
+                    dataset[f'{patient_id}_aug']['score'] = _score
+        return dataset
+    
+    def _split(self, dataset, dataset_type):
+        self.dataset = dataset
+        self.dataset_type = dataset_type
+        return deepcopy(self)
+    
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        patient_id = list(self.dataset.keys())[idx]
+        images = []
+        for _type in self.img_type:
+            images.append(self.dataset[patient_id]['image'][_type])
+        label = self.dataset[patient_id]['label']
+        score = self.dataset[patient_id]['score']
+        info = {}#{'patient_id':patient_id}#, 'img_type':[_type], }
+            
+        return images, label, score, info
+
 class Conv_Att_MCI_Dataset(Dataset):
-    def __init__(self, img_type='all', is_soft_label=False):
+    def __init__(self, img_type, is_soft_label=False):
+        self.img_type = img_type
         self.is_soft_label = is_soft_label
         self.dataset_raw = self.load_dataset()
-        self.load_images(img_type)
+        self.load_images()
 
     def load_dataset(self):
         image_folder = os.path.join(DATASET_DIR, 'images')
@@ -36,12 +162,10 @@ class Conv_Att_MCI_Dataset(Dataset):
             dataset[int(patient_id)]['score'] = moca_score
         return dataset
 
-    def load_images(self, img_type):
-        self.img_type = ['clock', 'trail', 'copy'] if len(img_type) == 1 and img_type[0] == 'all' else img_type
-
+    def load_images(self):
         transform_aug = transforms.Compose([
             transforms.Pad([12,12,12,12], fill=255), # left, top, right, bottom, fill=255 to match the background color of original images
-            transforms.RandomCrop([256,256])
+            transforms.RandomCrop([256,256]),
         ])
 
         transform = transforms.Compose([
--- models.py
+++ models.py
@@ -1,26 +1,31 @@
 import torch
-import torchvision.models as models
 from torch import nn
+import torch.nn.functional as F
+from torchvision.models import vgg16, VGG16_Weights
 
 class BaseModule(nn.Module):
     def __init__(self):
         super().__init__()
 
     def _get_img_type(self, img_type):
-        self._n_img_type = ['clock', 'trail', 'copy'] if len(img_type) == 1 and img_type[0] == 'all' else img_type
+        self.img_type = img_type
     
     def _build_backbone_vgg16(self, n_img_type, vgg16_freezing):
         self.vgg16_models = nn.ModuleList()
         for _ in range(n_img_type):
-            self.vgg16_models.append(models.vgg16(pretrained=True).features)
+            backbone = vgg16(weights=VGG16_Weights.IMAGENET1K_V1).features
             if vgg16_freezing:
-                for p in self.vgg16_models[-1].parameters():
+                for p in backbone.parameters():
                     p.requires_grad = False
+            self.vgg16_models.append(backbone)
 
     def _get_features(self, xs):
         x_cat = []
         for i, x in enumerate(xs):
             x = self.vgg16_models[i](x) # (B, C, H, W)
+            x = F.adaptive_avg_pool2d(x, [1,1])
+            x = x.squeeze()
+            assert len(x.shape) == 2
             x_cat.append(x)
         x = torch.cat(x_cat, -1)
         return x
@@ -32,13 +37,15 @@ class BaseModel(BaseModule):
        
     def build_network(self, img_type, vgg16_freezing):
         self._get_img_type(img_type)
-        n_img_type = len(self._n_img_type)
+        n_img_type = len(self.img_type)
         self._build_backbone_vgg16(n_img_type, vgg16_freezing)
 
         self.network = nn.Sequential(
-            nn.AdaptiveAvgPool2d([1,1]),
-            nn.Flatten(1),
-            nn.Linear(512,2)
+            nn.Linear(512*n_img_type,1024),
+            nn.ReLU(),
+            nn.Linear(1024,1024),
+            nn.ReLU(),
+            nn.Linear(1024,2)
         )
     
     def forward(self, xs):
@@ -53,13 +60,15 @@ class VGG16GradCAM(BaseModule):
         
     def build_network(self, img_type, vgg16_freezing):
         self._get_img_type(img_type)
-        n_img_type = len(self._n_img_type)
+        n_img_type = len(self.img_type)
         self._build_backbone_vgg16(n_img_type, vgg16_freezing)
         
         self.network = nn.Sequential(
-            nn.AdaptiveAvgPool2d([1,1]),
-            nn.Flatten(1),
-            nn.Linear(512,2)
+            nn.Linear(512*n_img_type,1024),
+            nn.ReLU(),
+            nn.Linear(1024,1024),
+            nn.ReLU(),
+            nn.Linear(1024,2)
         )
         
     # 훅(hook) 등록을 위한 함수
@@ -109,36 +118,39 @@ class ConvAttnModel(BaseModule):
         
     def build_network(self, img_type, n_heads, h_dim_fc, n_layers, vgg16_freezing):
         self._get_img_type(img_type)
-        n_img_type = len(self._n_img_type)
+        n_img_type = len(self.img_type)
         self._build_backbone_vgg16(n_img_type, vgg16_freezing)
         vgg_out_channels = self.vgg16_models[-1][-3].out_channels
+        self.cls_token = nn.Parameter(torch.randn(1, 1, self.h_dim_attn))
         
-        self.conv_1x1s, self.attns = nn.ModuleList(),nn.ModuleList()
+        self.conv_1x1s, self.attns = nn.ModuleList(), nn.ModuleList()
         for _ in range(n_img_type):
             self.conv_1x1s.append(nn.Conv2d(vgg_out_channels, self.h_dim_attn, 1))
 
             enc_layer = nn.TransformerEncoderLayer(self.h_dim_attn, n_heads, h_dim_fc, batch_first=True)    
             self.attns.append(nn.TransformerEncoder(enc_layer, n_layers))
-
+        
         self.network = nn.Sequential(
-            # nn.Linear(self.h_dim_attn*n_img_type, 2),
-            nn.Linear(self.h_dim_attn*n_img_type, self.h_dim_attn),
+            nn.Linear(self.h_dim_attn*n_img_type, 512),
             nn.ReLU(),
-            nn.Linear(self.h_dim_attn, self.h_dim_attn),
+            nn.Linear(512, 512),
             nn.ReLU(),
-            nn.Linear(self.h_dim_attn, 2)
+            nn.Linear(512, 2)
         )
     
     def forward(self, xs):
         x_cat = []
         for i,x in enumerate(xs):
+            # cls_token = torch.randn(x.shape[0], 1, self.h_dim_attn).to(x) #(B, 1, D)
+            # cls_token = torch.zeros(x.shape[0], 1, self.h_dim_attn).to(x) #(B, 1, D)
+            cls_token = self.cls_token.expand(x.shape[0], -1, -1)
+            
             x_tilde = self.vgg16_models[i](x) # (B, C, H, W)
             x_tilde = self.conv_1x1s[i](x_tilde) # (B, D, H, W)
-
-            _x_tilde = x_tilde.flatten(2).permute(0,2,1) #(B, HW, D)
-            cls_rand_token = torch.randn(_x_tilde.shape[0], 1, self.h_dim_attn).to(_x_tilde) #(B, 1, D)
-            # cls_rand_token = torch.zeros(_x_tilde.shape[0], 1, self.h_dim_attn).to(_x_tilde) #(B, 1, D)
-            cls_x_tilde = torch.cat([cls_rand_token, _x_tilde], 1) # (B, HW+1, D)
+            
+            x_tilde_reshaped = x_tilde.flatten(2).permute(0,2,1) #(B, HW, D)
+            
+            cls_x_tilde = torch.cat([cls_token, x_tilde_reshaped], 1) # (B, HW+1, D)
             x_attn = self.attns[i](cls_x_tilde) # (B, HW+1, D)
 
             x = x_attn[:,0] #(B, D)
